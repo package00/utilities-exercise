ac1dd14d28eb965d48c27f737a527b11faf79b68
package <femisage@gmail.com>
2022-05-23 02:37:38 -0500

    utilities exercise solution

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const lib = require('./Utilities/file1');
+const col = require('./Utilities/file2');
 
+numberArray = [18,09,1,23,47,5,31,3];
+secondNumberArray = [10,11,12];
+
+let highest = lib.largeNumber(numberArray);
+
+function Sum(arr){
+	let total = col.sum(arr);
+	return total;
+}
+
+console.log("The highest number is: ",highest);
+console.log("Remove the 3rd item from array: ", lib.cut3(numberArray));
+console.log("Add numbers in an array: ", Sum(numberArray));
+console.log("concatenate two arrays: ", col.concat(numberArray,secondNumberArray));
